9c7f6ef8b5c5e682c9c79f50e0675f09bc9a8fe8
hong <shizhouzhouya@gmail.com>
2026-04-09 20:34:19 +0800

    refactor(service): 优化设备绑定流程并新增名称修改业务逻辑 1.逻辑精简：重构 BindDevice，将原有的“查询-校验-更新”逻辑简化为直接调用仓库层的原子操作，利用数据库约束保证并发安全性。 2.功能实现：新增 UpdateDeviceName 业务方法，严格执行“查询设备-所有权校验-持久化更新”的业务链路，确保只有设备拥有者可修改名称。 3.鲁棒性增强：在解绑与改名逻辑中，强化了对 ownerID 的判空与权限校验，防止非法操作。 4.代码清理：移除冗余的调试日志与被注释的废弃逻辑，保持 Service 层代码整洁。

--- internal/service/device.go
+++ internal/service/device.go
@@ -5,7 +5,6 @@ import (
 	"IOTProject/pkg/error_code"
 	"context"
 	"errors"
-	"log"
 
 	"gorm.io/gorm"
 )
@@ -21,20 +20,25 @@ func NewDeviceService(repo domain.DeviceRepository) *DeviceService {
 }
 func (s *DeviceService) BindDevice(ctx context.Context, req *domain.BindDeviceResp) error {
 	//查看设备拥有者
-	ownerID, err := s.repo.GetDeviceOwner(ctx, req.DeviceUID)
+	//Info, err := s.repo.GetDeviceInfoByUID(ctx, req.DeviceUID)
+	//if err != nil {
+	//	if errors.Is(err, gorm.ErrRecordNotFound) {
+	//		return error_code.DeviceNotFound
+	//	}
+	//	return err
+	//}
+	//设备被其他人绑定
+	//if Info.UserID != nil {
+	//	log.Printf("[DEBUG] 设备 %s 已被绑定, 拥有者 ID: %d", req.DeviceUID, Info.UserID)
+	//	return error_code.DeviceIsBind
+	//}
+	//return s.repo.BindDevice(ctx, req)
+
+	err := s.repo.BindDevice(ctx, req)
 	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return error_code.DeviceNotFound
-		}
 		return err
 	}
-	//设备被其他人绑定
-	if ownerID != nil {
-
-		log.Printf("[DEBUG] 设备 %s 已被绑定, 拥有者 ID: %d", req.DeviceUID, *ownerID)
-		return error_code.DeviceIsBind
-	}
-	return s.repo.BindDevice(ctx, req)
+	return nil
 }
 
 // UnBindDevice 执行设备解绑逻辑
@@ -48,7 +52,6 @@ func (s *DeviceService) UnBindDevice(ctx context.Context, req *domain.UnbindDevi
 		}
 		return err
 	}
-	// 权限校验
 	if ownerID == nil {
 		return error_code.DeviceNotBind
 	}
@@ -68,4 +71,16 @@ func (s *DeviceService) UnBindDevice(ctx context.Context, req *domain.UnbindDevi
 // GetDeviceInfo 获取指定用户的设备资产列表
 func (s *DeviceService) GetDeviceInfo(ctx context.Context, userID int64) (*domain.DeviceInfo, error) {
 	return s.repo.GetDeviceInfo(ctx, userID)
+}
+
+// UpdateDeviceName 更新指定设备的名称。
+func (s *DeviceService) UpdateDeviceName(ctx context.Context, req *domain.UpdateDeviceNameRequest) error {
+	device, err := s.repo.GetDeviceInfoByUID(ctx, req.DeviceUID)
+	if err != nil {
+		return err
+	}
+	if device.UserID == nil || *device.UserID != req.UserID {
+		return error_code.NotDeviceOwner
+	}
+	return s.repo.UpdateDeviceName(ctx, req.DeviceUID, req.DeviceName)
 }
